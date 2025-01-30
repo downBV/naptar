7d47a968ba2d6ac9832553a49ed6f6bec3b34d49
downBV <bvik159@gmail.com>
2025-01-30 23:10:13 +0100

    Update service-worker.js

--- update/service-worker.js
+++ update/service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'muszakrend-cache-v1';
 const urlsToCache = [
-  '/naptar/',
-  '/naptar/index.html',
-  '/naptar/icon.png',
-  '/naptar/manifest.json'
+  '/naptar/update/',
+  '/naptar/update/update.html',
+  '/naptar/update/icon.png',
+  '/naptar/update/manifest.json'
 ];
 
 self.addEventListener('install', event => {
